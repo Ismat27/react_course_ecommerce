3e718ebdb22003b9a6cd7b43585501e0db4b49c7
ismat27 <adegbiteismail27@gmail.com>
2022-11-05 02:51:38 -0700

    update structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute
+} from './pages'
+
+import { Navbar, Sidebar, Footer } from "./components";
 
 function App() {
   return (
-     <h4>comfy sloth starter</h4>
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/products/:id" element={<SingleProduct/>} />
+
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/cart" element={<Cart/>} />
+
+        <Route path="/about" element={<About/>} />
+        <Route path="*" element={<Error/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+    
   );
 }
 

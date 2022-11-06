faffbc281cb6c31cc65dfffa556ff380427ea6f0
ismat27 <adegbiteismail27@gmail.com>
2022-11-06 01:47:15 -0700

    updates

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,8 +1,29 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const ProductImages = () => {
-  return <h4>product images</h4>
+const ProductImages = ({images = [{url: ''}]}) => {
+
+  const [main, setMain] = useState(images[0])
+  return (
+    <Wrapper>
+      <img src={main.url} alt='product image' />
+      <div className='gallery'>
+        {
+          images.map((image, index) => {
+            return (
+              <img 
+                key={index}
+                src={image.url} 
+                alt='product image' 
+                onClick={() => setMain(images[index])}
+                className={`${image.url === main.url? 'active': null }`}
+              /> 
+            )
+          })
+        }
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`

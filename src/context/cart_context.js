4f8dc37b5babda156ef7fb8d3d3db85b10b9d3b8
ismat27 <adegbiteismail27@gmail.com>
2023-01-15 04:59:52 -0800

    store cart items into local storage. And set the stored data as initial value of cart items

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,16 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart')
+  if (cart) {
+    return JSON.parse(cart)
+  }
+  else return []
+}
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -23,6 +31,10 @@ export const CartProvider = ({ children }) => {
   const addToCart = (id, color, amount, product) => {
     dispatch({type: ADD_TO_CART, payload: {id, color, amount, product}})
   }
+
+  useEffect(()=> {
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
 
   return (
     <CartContext.Provider value={
